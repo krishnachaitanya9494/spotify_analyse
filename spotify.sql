83c1d4de8085de8d6d250fa9982e9c2077e90e88
krishnachaitanya9494 <139668808+krishnachaitanya9494@users.noreply.github.com>
2025-07-24 08:33:33 +0530

    Update spotify.sql

--- spotify.sql
+++ spotify.sql
@@ -4,3 +4,13 @@ select  artists, album_name,popularity , duration_ms from spotify where artists 
 
 -----------top 1 popular track in the dataset-------
 select top 1 track_id , artists , popularity  from spotify order by popularity desc
+
+select * from spotify
+-------------------intermediate sql queries---------
+----top 10 longest explicit tracks---
+
+
+select top 10  artists , album_name , track_name from spotify where explicit = '0'
+-----tracks suitable for dancing----
+
+select danceability , artists , track_id , popularity , duration_ms from spotify order by duration_ms desc
